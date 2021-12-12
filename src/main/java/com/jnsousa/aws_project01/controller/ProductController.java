cd06f14b7401f21a3736969b5c894acb45f0839f
Jairo <jaironsousa@gmail.com>
2021-12-12 17:39:13 -0300

    Criando o controller de Produtos

---
+++ src/main/java/com/jnsousa/aws_project01/controller/ProductController.java
@@ -0,0 +1,80 @@
+package com.jnsousa.aws_project01.controller;
+
+import com.jnsousa.aws_project01.model.Product;
+import com.jnsousa.aws_project01.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 12/12/2021 - 17:27
+ */
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping
+    public Iterable<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Product> findById(@PathVariable("id") long id) {
+        Optional<Product> optProduct = productRepository.findById(id);
+        if (optProduct.isPresent()) {
+            return new ResponseEntity<Product>(optProduct.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> saveProduct(@RequestBody Product product) {
+        Product productCreated = productRepository.save(product);
+        return new ResponseEntity<Product>(productCreated, HttpStatus.CREATED);
+    }
+
+    @PutMapping(path = "/{ìd}")
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product, @PathVariable("id") long id) {
+        if (productRepository.existsById(id)) {
+            product.setId(id);
+            Product productUpdated = productRepository.save(product);
+            return new ResponseEntity<Product>(productUpdated, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping(path = "/{ìd}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable("id") long id) {
+        Optional<Product> optProduct = productRepository.findById(id);
+        if (optProduct.isPresent()) {
+            Product product = optProduct.get();
+            productRepository.delete(product);
+            return new ResponseEntity<Product>(product, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping(path = "/bycode")
+    public ResponseEntity<Product> findByCode(@RequestParam String code) {
+        Optional<Product> optProduct = productRepository.findByCode(code);
+        if (optProduct.isPresent()) {
+            return new ResponseEntity<Product>(optProduct.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
